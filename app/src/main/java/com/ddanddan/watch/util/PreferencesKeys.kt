ed58c229f85ca3b7069bc26002ab04c0e7d2fada
nami <dnska6657@naver.com>
2024-11-17 22:06:49 +0900

    [add/#10] PreferencesKey 추가

--- app/src/main/java/com/ddanddan/watch/util/PreferencesKeys.kt
+++ app/src/main/java/com/ddanddan/watch/util/PreferencesKeys.kt
@@ -1,8 +1,7 @@
 package com.ddanddan.watch.util
 
-import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.stringPreferencesKey
 
 object PreferencesKeys {
-    val ACCESS_TOKEN_KEY: Preferences.Key<String> = stringPreferencesKey("access_token")
+    val ACCESS_TOKEN_KEY = stringPreferencesKey("access_token")
 }
